3df17a2b2e093c4b2bc6a5c9cbae07d647c9574d
Svitlana <svetagimish@gmail.com>
2026-05-16 11:24:21 +0300

    fix modal

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { fetchNoteById } from "@/lib/api";
+import Modal from "@/components/Modal/Modal";
 import css from "./NotePreview.module.css";
 
 export default function NotePreviewClient({ id }: { id: string }) {
@@ -11,21 +12,35 @@ export default function NotePreviewClient({ id }: { id: string }) {
   const { data, isLoading, isError } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
   });
 
-  if (isLoading) return <p>Loading...</p>;
-  if (isError || !data) return <p>Error</p>;
+  const handleClose = () => router.back();
+
+  if (isLoading)
+    return (
+      <Modal onClose={handleClose}>
+        <p>Loading...</p>
+      </Modal>
+    );
+  if (isError || !data)
+    return (
+      <Modal onClose={handleClose}>
+        <p>Error loading note</p>
+      </Modal>
+    );
 
   return (
-    <div className={css.backdrop} onClick={() => router.back()}>
-      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
-        <h2>{data.title}</h2>
-        <p>{data.tag}</p>
-        <p>{data.content}</p>
-        <p>{data.createdAt}</p>
-
-        <button onClick={() => router.back()}>Close</button>
+    <Modal onClose={handleClose}>
+      <div className={css.container}>
+        <h2 className={css.item}>{data.title}</h2>
+        <p className={css.tag}>{data.tag}</p>
+        <p className={css.content}>{data.content}</p>
+        <p className={css.date}>{data.createdAt}</p>
+        <button className={css.backBtn} onClick={() => router.back()}>
+          Close
+        </button>
       </div>
-    </div>
+    </Modal>
   );
 }
